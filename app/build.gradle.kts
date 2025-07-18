73448fabb027fa206e3f50b86c7f8c4237958355
Keronei <keronei95@gmail.com>
2025-07-18 16:08:00 +0300

    Add Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id(BuildPlugins.ktlintPlugin)
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -23,12 +24,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8" // Or '11'
+        jvmTarget = "11" // Or '11'
     }
 
     testOptions {
@@ -48,6 +49,7 @@ android {
     namespace = "keronei.swapper"
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 
     dependencies {
@@ -91,6 +93,24 @@ android {
         ksp("androidx.room:room-compiler:$room_version")
         annotationProcessor("androidx.room:room-compiler:$room_version")
         implementation("androidx.room:room-ktx:$room_version")
+
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+        implementation("androidx.activity:activity-compose:1.10.1")
+        implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+        implementation("androidx.compose.ui:ui")
+        implementation("androidx.compose.ui:ui-graphics")
+        implementation("androidx.compose.ui:ui-tooling-preview")
+        implementation("androidx.compose.material3:material3")
+        androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+        debugImplementation("androidx.compose.ui:ui-tooling")
+        debugImplementation("androidx.compose.ui:ui-test-manifest")
+        implementation("androidx.legacy:legacy-support-v4:1.0.0")
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.2")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
+        implementation("androidx.fragment:fragment-ktx:1.8.8")
+        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+
 
         androidTestImplementation(TestLibraries.testRunner)
         androidTestImplementation(TestLibraries.espresso)
