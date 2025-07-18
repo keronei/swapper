8bd91ea4ddd45d9219bef4304b73ca663079b5e7
Keronei <keronei95@gmail.com>
2025-07-18 16:05:58 +0300

    Unify colors

---
+++ app/src/main/java/keronei/swapper/dashboard/dispatch/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package keronei.swapper.dashboard.dispatch.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xff24ba10)
+val PurpleGrey80 = Color(0xFF143f05)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF24ba10)
+val PurpleGrey40 = Color(0xFF143f05)
+val Pink40 = Color(0xFF7D5260)
